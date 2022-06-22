920a3a1ded06df5742e67813f345d8235398e2a1
umluks <lucas.sga@gmail.com>
2022-06-22 05:51:06 -0300

    atualizacao arquivo banco.js

--- curso-js/banco.js
+++ curso-js/banco.js
@@ -3,21 +3,42 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class bancoCliente {
     agencia;
+    contaCorrente;
     saldo;
+
+    saqueCc(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            console.log(bancoCliente1.saldo, "valor disponivel na conta")
+        } else {
+        console.log("Voce nao possui saldo suficiente para saque")
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
     cliente1.nome = "Lucas";
     cliente1.cpf = 12345678901;
-    cliente1.agencia = 0001;
-    cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
     cliente2.nome = "Maria";
     cliente2.cpf = 98765432102;
-    cliente2.agencia = 0001;
-    cliente2.saldo = 0;
 
-console.log (cliente1, cliente2)
+const bancoCliente1 = new bancoCliente();
+    bancoCliente1.agencia = 0001;
+    bancoCliente1.contaCorrente = 1001
+    bancoCliente1.saldo = 0;
+
+console.log (cliente1);
+console.log (cliente2);
+console.log (bancoCliente1)
+
+// Operacoes de Deposito e Saque
+bancoCliente1.saldo = 1000 //adicionando saldo ao cliente
+console.log(bancoCliente1.saldo)
+
+bancoCliente1.saqueCc(2000) //operacao saqueCC
